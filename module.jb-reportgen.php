654ab282792848d6a9ee2bfbeb80b2cc2bc48301
jbostoen <jbostoen@outlook.com>
2020-09-10 12:59:27 +0200

    Version 20200810-1259
    *subrepo

---
+++ module.jb-reportgen.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @copyright   Copyright (C) 2019-2020 Jeffrey Bostoen
+ * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @version     2020-04-09 16:58:14
+ *
+ * iTop module definition file
+ */
+ 
+SetupWebPage::AddModule(
+        __FILE__, // Path to the current file, all other file names are relative to the directory containing this file
+        'jb-reportgen/2.6.200409',
+        array(
+                // Identification
+                //
+                'label' => 'Feature: report generator',
+                'category' => 'business',
+
+                // Setup
+                //
+                'dependencies' => array( 
+                ),
+                'mandatory' => false,
+                'visible' => true,
+
+                // Components
+                //
+                'datamodel' => array(
+					// 'model.jb-reportgen.php',
+					'core/PopupMenuExtensionReportGenerator.class.inc.php',
+					'core/iReportTool.int.inc.php',
+					'core/RTParent.class.inc.php',
+					'core/RTTwig.class.inc.php',
+					'core/RTPDF.class.inc.php',
+                ),
+                'webservice' => array(
+
+                ),
+                'data.struct' => array(
+                        // add your 'structure' definition XML files here,
+                ),
+                'data.sample' => array(
+                        // add your sample data XML files here,
+                ),
+
+                // Documentation
+                //
+                'doc.manual_setup' => '', // hyperlink to manual setup documentation, if any
+                'doc.more_information' => '', // hyperlink to more information, if any
+
+                // Default settings
+                //
+                'settings' => array(
+                        // Module specific settings go here, if any
+                ),
+        )
+);
---
+++ nl.dict.jb-reportgen.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @copyright   Copyright (C) 2019-2020 Jeffrey Bostoen
+ * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * Localized data
+ */
+
+Dict::Add('NL NL', 'Dutch', 'Dutch', array(
+
+	'UI:Menu:ReportGenerator:ShowReportTitleMissing' => htmlspecialchars('<title>').'-tag ontbreekt'
+	
+));
